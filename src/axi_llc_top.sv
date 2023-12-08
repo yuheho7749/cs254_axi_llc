4f5375153ec3a3469bcecf57d11a53a50cd60ebd
crdgdr <q_li@ucsb.edu>
2023-12-08 12:27:04 -0800

    changes

--- src/axi_llc_top.sv
+++ src/axi_llc_top.sv
@@ -440,24 +440,24 @@ module axi_llc_top #(
 
   // Isolation module before demux to easy flushing,
   // AXI requests get stalled while flush is active
-  axi_isolate #(
-    .NumPending     ( axi_llc_pkg::MaxTrans ),
-    .AxiAddrWidth   ( AxiAddrWidth          ),
-    .AxiDataWidth   ( AxiDataWidth          ),
-    .AxiIdWidth     ( AxiIdWidth            ),
-    .AxiUserWidth   ( AxiUserWidth          ),
-    .axi_req_t      ( slv_req_t             ),
-    .axi_resp_t     ( slv_resp_t            )
-  ) i_axi_isolate_flush (
-    .clk_i,
-    .rst_ni,
-    .slv_req_i,  // Slave port request
-    .slv_resp_o, // Slave port response
-    .mst_req_o  ( to_demux_req  ),
-    .mst_resp_i ( to_demux_resp ),
-    .isolate_i  ( llc_isolate   ),
-    .isolated_o ( llc_isolated  )
-  );
+  // axi_isolate #(
+  //   .NumPending     ( axi_llc_pkg::MaxTrans ),
+  //   .AxiAddrWidth   ( AxiAddrWidth          ),
+  //   .AxiDataWidth   ( AxiDataWidth          ),
+  //   .AxiIdWidth     ( AxiIdWidth            ),
+  //   .AxiUserWidth   ( AxiUserWidth          ),
+  //   .axi_req_t      ( slv_req_t             ),
+  //   .axi_resp_t     ( slv_resp_t            )
+  // ) i_axi_isolate_flush (
+  //   .clk_i,
+  //   .rst_ni,
+  //   .slv_req_i,  // Slave port request
+  //   .slv_resp_o, // Slave port response
+  //   .mst_req_o  ( to_demux_req  ),
+  //   .mst_resp_i ( to_demux_resp ),
+  //   .isolate_i  ( llc_isolate   ),
+  //   .isolated_o ( llc_isolated  )
+  // );
 
   axi_demux #(
     .AxiIdWidth     ( AxiCfg.SlvPortIdWidth  ),
@@ -466,8 +466,8 @@ module axi_llc_top #(
     .b_chan_t       ( slv_b_chan_t           ),
     .ar_chan_t      ( slv_ar_chan_t          ),
     .r_chan_t       ( slv_r_chan_t           ),
-    .axi_req_t      ( slv_req_t              ),
-    .axi_resp_t     ( slv_resp_t             ),
+    .req_t          ( slv_req_t              ),
+    .resp_t         ( slv_resp_t             ),
     .NoMstPorts     ( 32'd2                  ),
     .MaxTrans       ( axi_llc_pkg::MaxTrans  ),
     .AxiLookBits    ( axi_llc_pkg::UseIdBits ),
